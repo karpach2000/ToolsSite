65ac781051097c46c46cdfa791fc8b8788ff9a87
dmitry <karpach2000@yandex.ru>
2018-12-22 19:50:34 +0300

    Работает

--- src/main/resources/static/web/javascript/unitsWs.js
+++ src/main/resources/static/web/javascript/unitsWs.js
@@ -8,9 +8,33 @@ function showMenuContent(id) {
     document.getElementById(currentMenuCountingItem).hidden=true;
     document.getElementById(id).hidden=false;
     currentMenuCountingItem=id;
+
 }
 
 
+function ropeSlack() {
+    try {
+        var l = document.getElementById("l").value;
+        var h = document.getElementById("h").value;
+        var g0 = document.getElementById("g0").value;
+        var Ta = document.getElementById("Ta").value/10;
+        var Tb = Ta - g0*h;
+        var Tcp = (Ta + Tb)/2;
 
+        var tB = h/l;
+        var L = Math.sqrt(h*h + l*l);
+        var cB = l/L;
+
+        var H = Tcp*cB;
+        var fmax = g0 * l*l /(8*H*cB);
+        var Hs = (Tcp - g0*fmax)*cB
+        var fmaxs = fmax*H/Hs
+
+        document.getElementById("notationResult").textContent = fmaxs;
+    }
+    catch (e) {
+        document.getElementById("notationResult").textContent="Данные введены не верно."
+    }
+}
 
 
