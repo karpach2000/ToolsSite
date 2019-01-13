5efabd81a6aaa1c7cf9cbb4611a1bbc2d979f0f0
Павел Чурляев <churljaev.p@lokasoft.ru>
2019-01-13 23:33:46 +0300

    Роли переименованы, чтобы спринг не ругался

--- src/main/resources/db/migration/2019_01_13_19_33__Users_roles.sql
+++ src/main/resources/db/migration/2019_01_13_19_33__Users_roles.sql
@@ -3,8 +3,8 @@ CREATE TABLE Roles(
 	role VARCHAR(64)
 );
 
-INSERT INTO Roles(id, role) VALUES(1, "ADMIN");
-INSERT INTO Roles(id, role) VALUES(2, "USER");
+INSERT INTO Roles(id, role) VALUES(1, "ROLE_ADMIN");
+INSERT INTO Roles(id, role) VALUES(2, "ROLE_USER");
 
 CREATE TABLE Users_roles(
 	user_id INTEGER,
